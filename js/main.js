72680d5ac946f7db8bf2e3a9a5285335fceed3f5
goldenpencil <georgeisaac603@gmail.com>
2019-10-02 08:49:59 +0100

    final countdown extended

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@
      * ------------------------------------------------------ */
     var ssFinalCountdown = function () {
 
-        var finalDate = '2019/10/01';
+        var finalDate = '2019/10/10';
 
         $('.home-content__clock').countdown(finalDate)
             .on('update.countdown finish.countdown', function (event) {
